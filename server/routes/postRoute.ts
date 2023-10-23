cd44bb39cf03fd208597ce77fc0fda19ef2d876f
CalyWorld <obinnaanizoba5@gmail.com>
2023-10-23 15:16:18 +0800

    fixed the post route naming convention

--- server/routes/postRoute.ts
+++ server/routes/postRoute.ts
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const postController = require("../models/post");
+const postController = require("../controllers/postController");
 
 //get all posts
 router.get("/", postController.getAllPosts);
 //get a specific post
-router.get("/post/:id", postController.getPostById);
+router.get("/:id", postController.getPostById);
 //create a post
-router.post("/post");
+router.post("/create", postController.createPost);
 //edit a specific post
-router.put("/post/:id");
+router.put("/:id/update");
 //delete a post
-router.delete("/post/:id");
+router.delete("/:id/delete");
 
 module.exports = router;
