9d88b1602340733d613a11caf52fdaf10fed6374
CalyWorld <obinnaanizoba5@gmail.com>
2024-02-01 06:55:59 +0800

    update

--- server/dist/routes/signUpRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-const signUpController = require("../controllers/signUpController");
-router.post("/", signUpController.signUpPost);
-module.exports = router;
